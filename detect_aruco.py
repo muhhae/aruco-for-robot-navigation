d1865848450efad516afa2759b3f8dc77842b95e
muhhae <haekal.muhamat@gmail.com>
2024-10-17 21:06:25 +0700

    up

--- detect_aruco.py
+++ detect_aruco.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 
 
 def main():
@@ -8,32 +7,17 @@ def main():
     detector = cv2.aruco.ArucoDetector(dictionary, param)
     vid = cv2.VideoCapture(0)
 
-    start_time = time.time()
-    frame_counter = 0
-
     while 1:
-        frame_counter += 1
         ret, frame = vid.read()
         if ret is False:
             break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        corners, ids, rejected = detector.detectMarkers(gray)
+        corners, ids, rejected = detector.detectMarkers(frame)
         if ids is not None:
             cv2.aruco.drawDetectedMarkers(frame, corners, ids)
 
-        fps = frame_counter / (time.time() - start_time)
-        cv2.putText(
-            frame,
-            f"FPS: {fps:.3f}",
-            (30, 30),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 255, 255),
-            2,
-            cv2.LINE_AA,
-        )
         cv2.imshow("Detected Markers", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
