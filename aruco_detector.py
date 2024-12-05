80f9bf1d5f0c704176e426977b7b73ba672bfb14
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2024-12-06 05:49:14 +0700

    feat: control

--- aruco_detector.py
+++ aruco_detector.py
@@ -85,6 +85,7 @@ class Object:
 
 
 class ArucoDetector:
+    current_move = None
     controller: RobotControl
     frame: cv2.UMat
     aruco_dict: cv2.aruco.Dictionary
@@ -178,7 +179,7 @@ class ArucoDetector:
         if self.current_position is None:
             return
 
-        n = distance - 20
+        n = distance - 30
         if abs(n) < 1:
             print("Exactly at", self.current_position.id)
             next_id_index = self.routes.index(self.current_position.id) + 1
@@ -189,24 +190,22 @@ class ArucoDetector:
             R = self.orientation_dict[Direction.R][self.orientation]
             if self.current_position.neighbour[T] == next_id:
                 print("Move Forward")
-                self.controller.robot_forward()
+                self.current_move = "Maju"
             elif self.current_position.neighbour[L] == next_id:
                 print("Move Left")
-                self.controller.robot_pivot_left()
+                self.current_move = "Kiri"
             elif self.current_position.neighbour[R] == next_id:
                 print("Move Right")
-                self.controller.robot_pivot_right()
+                self.current_move = "Kanan"
             elif self.current_position.neighbour[B] == next_id:
                 print("Move Backward")
-                self.controller.robot_backward()
+                self.current_move = "Mundur"
         elif n > 0:
             print("Approaching", self.current_position.id)
             print("Move Forward")
-            self.controller.robot_forward()
         else:
             print("Moving away from", self.current_position.id)
             print("Move Backward")
-            self.controller.robot_backward()
 
     def ProcessArucoTransform(self, id, dis, dir):
         aruco_marker = None
@@ -239,7 +238,15 @@ class ArucoDetector:
                 id, dis, dir = self.GetPosition(aruco_transforms)
                 self.ProcessArucoTransform(id, dis, dir)
                 self.CurrentTask(dis)
-
+            if self.current_move is not None:
+                if self.current_move == "Maju":
+                    self.controller.robot_forward()
+                elif self.current_move == "Mundur":
+                    self.controller.robot_backward()
+                elif self.current_move == "Kanan":
+                    self.controller.robot_turn_right()
+                elif self.current_move == "Kiri":
+                    self.controller.robot_pivot_left()
             self.frame = frame.copy()
             cv2.imshow("frame", self.frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
